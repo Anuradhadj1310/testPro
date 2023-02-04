ad030d65cf84e874027215b5097ee01e1decba01
Sharanyadj <jagtapsharanyad@gmail.com>
2023-02-04 20:21:21 +0530

    try changing existing file

--- src/Addition.java
+++ src/Addition.java
@@ -5,17 +5,34 @@ public class Addition
 		public static void main(String args[])
 		{
 			int a=0,b=0,res;
-			System.out.println("Eter value for A :");
-			Scanner sc= new Scanner(System.in);
+			try {
+					System.out.println("Enter value for A :");
+					Scanner sc= new Scanner(System.in);
+					
 			
-			a=sc.nextInt();
-	
-			System.out.println("Eter value for B :");
-			sc= new Scanner(System.in);
+					if(sc.hasNextInt())
+						{
+							a=sc.nextInt();
+							
+						}
 			
-			b=sc.nextInt();
 			
-			res=a+b;
-			System.out.println("REs = "+res);
+					System.out.println("Enter value for B :");
+					Scanner sc1= new Scanner(System.in);
+					
+					if(sc1.hasNextInt())
+						{
+							b=sc1.nextInt();
+							
+						}
+					sc.close();
+					sc1.close();
+					res=a+b;
+					System.out.println("Res = "+res);
+			}
+			catch(Exception e)
+			{
+				System.out.println("Wrong input");
+			}
 		}
 }
